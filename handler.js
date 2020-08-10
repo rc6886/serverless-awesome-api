4730f47715d1cdc80a7b2b5a6988b53e3d7f7af6
Rodney Coleman <rc6886@gmail.com>
2020-08-10 09:11:37 -0500

    Making another change.

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go 2Serverless v1.0! Your function executed successfully!',
+        message: 'Go jhdl;akfjhsad;lkjf;alskdjf v1.0! Your function executed successfully!',
         input: event,
       },
       null,
